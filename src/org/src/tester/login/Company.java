e03560098a99e8dcb38c4bbc79be3956eef2fea0
vinodhinibabu <vinodhinibabu19@gamil.com>
2020-01-31 19:16:19 +0530

    recoded

--- src/org/src/tester/login/Company.java
+++ src/org/src/tester/login/Company.java
@@ -20,7 +20,9 @@ for(int i=0; i<10; i++) {
 }
 int sum=a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9];
 System.err.println("enter the sum of values:" +sum);
-System.out.println("code modified");
 System.out.println("code changed");
+System.out.println("code modified");
+System.out.println("abc");
+System.err.println("xyz");
 }
 }
