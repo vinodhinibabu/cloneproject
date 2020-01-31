69ccf4fb13f5d40f7fecc2ecc3081afd8d1091b0
vinodhinibabu <vinodhinibabu19@gamil.com>
2020-01-31 19:11:10 +0530

    code modified

--- src/org/src/tester/login/Company.java
+++ src/org/src/tester/login/Company.java
@@ -20,8 +20,7 @@ for(int i=0; i<10; i++) {
 }
 int sum=a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9];
 System.err.println("enter the sum of values:" +sum);
-float average=sum/10;
-System.err.println("enter the average of values:" +average);
-
+System.out.println("code modified");
+System.out.println("code changed");
 }
 }
